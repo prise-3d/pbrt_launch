fd2e2a78673f951239c00fb398f6ae49d2121a8e
sam <sam@prise3d>
2024-02-15 15:25:40 +0100

    fix: suppress opencv dependancy

--- exr_stats.py
+++ exr_stats.py
@@ -1,24 +1,9 @@
 """ Module pour extraire des informations d'un fichier EXR """
 
 import OpenEXR
-import os
-os.environ["OPENCV_IO_ENABLE_OPENEXR"]="1"
-import cv2
-
 import numpy as np
 
 print("start")
-
-def exr_stats2(filename):
-    print("start exr_stats2")
-    img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE | cv2.IMREAD_ANYDEPTH | cv2.EXR_TYPE_HALF)
-    print(img.shape)
-    # Loop over all pixels to display their value
-    for i in range(img.shape[0]):
-        for j in range(img.shape[1]):
-            pixel_value = img[i, j]
-            if (i<2 and j<2):                
-                print(f"Pixel at position ({i}, {j}): {pixel_value}")
 
 def exr_stats(filename):
     # Read the EXR image
@@ -44,5 +29,4 @@ def exr_stats(filename):
 
 if __name__ == "__main__":
     print("test for exr_stats")
-    exr_stats2("test_data/bmw-m6.exr")
     exr_stats("test_data/bmw-m6-Integrators_Non_contributing_rays.exr")
