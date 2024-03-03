0f365616f81ad88e4e3369c11eb0985e8ef602c1
Samuel Delepoulle <samuel.delepoulle@univ-littoral.fr>
2024-03-03 10:07:50 +0100

    refacto

--- analysis.py
+++ analysis.py
@@ -2,51 +2,39 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+# Function to generate a barplot to visualize the variable for each combination of two columns
+def generate_barplot(df, x_column, y_column, hue_column, title):
+    plt.figure(figsize=(12, 6))
+    sns.barplot(x=x_column, y=y_column, hue=hue_column, data=df, errorbar=None)
+    plt.title(title)
+    plt.xlabel(x_column)
+    plt.ylim(df[y_column].min(), df[y_column].max())
+    plt.xticks(rotation=45, ha='right')
+    plt.gca().set_xticklabels(sorted(df[x_column].unique()))
+    plt.ylabel(y_column)
+    plt.xticks(rotation=45)
+    plt.legend(title=hue_column)
+    plt.tight_layout()
+    plt.savefig(f"effect_of_{x_column.lower().replace(' ', '_')}_and_{hue_column.lower().replace(' ', '_')}.png")
+
 def analyze_effects(csv_file):
     # Read the CSV file into a pandas DataFrame
     df = pd.read_csv(csv_file)
 
     # Analyze the effects of the first three columns on the last column
     fourth_column_name = df.columns[3]
-    plt.figure(figsize=(12, 6))
 
     # Create a barplot to visualize the average execution time for each combination of Scene, Sampler, and Integrator
-    sns.barplot(x="Scene", y=fourth_column_name, hue="Sampler", data=df, errorbar=None)
-    plt.title("Effect of Scene and Sampler on " + fourth_column_name)
-    plt.xlabel("Scene")
-    plt.xticks(rotation=45, ha='right')
-    plt.gca().set_xticklabels(sorted(df['Scene'].unique()))
-    plt.ylabel(fourth_column_name)
-    plt.xticks(rotation=45)
-    plt.legend(title="Sampler")
-    plt.tight_layout()
-
-    # Save the plot to a file or display it
-    plt.savefig("effect_of_scene_and_sampler.png")
-
-    # Analyze the effects of the first three columns on the last column
-    sns.set(style="whitegrid")
-    plt.figure(figsize=(12, 6))
+    generate_barplot(df, "Scene", fourth_column_name, "Sampler", "Effect of Scene and Sampler on " + fourth_column_name)
 
     # Create a barplot to visualize the average execution time for each combination of Scene, Sampler, and Integrator
-    sns.barplot(x="Scene", y=fourth_column_name, hue="Integrator", data=df, errorbar=None)
-    plt.title("Effect of Scene and Integrator on " + fourth_column_name)
-    plt.xlabel("Scene")
-    plt.xticks(rotation=45, ha='right')
-    plt.gca().set_xticklabels(sorted(df['Scene'].unique()))
-    plt.ylabel(fourth_column_name)
-    plt.xticks(rotation=45)
-    plt.legend(title="Integrator")
-    plt.tight_layout()
+    generate_barplot(df, "Scene", fourth_column_name, "Integrator", "Effect of Scene and Integrator on " + fourth_column_name)
 
-    # Save the plot to a file or display it
-    plt.savefig("effect_of_scene_and_integrator.png")
     print("done")
-
 
 def main():
     # Specify the path to the CSV file
-    csv_file = "./output.csv"
+    csv_file = "stat_2024-02-22_15-30-27.csv"
 
     # Call the analyze_effects function with the CSV file path
     analyze_effects(csv_file)
